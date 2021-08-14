169fd013a32b120d6abe3917a89ac3b0816a69b9
Nicolas Walcker <walcker_@outlook.com>
2021-08-14 16:37:04 -0300

    feat/fix: cria funcao p/ limpar tela/ajusta botao

--- main.js
+++ main.js
@@ -1,25 +1,46 @@
 const button = document.querySelector(".js-button");
 const question = document.querySelector(".js-question");
 const answer = document.querySelector(".js-answer");
+var activeRiddle;
+var timer, timer2;
 
 const showData = (result) => {
-  let randomRiddleIndex = Math.floor(Math.random() * result.piadas.length);
-  let activeRiddle = result.piadas[randomRiddleIndex];
+  clearTimeout(timer);
+  let randomRiddleIndex;
+  do {
+    randomRiddleIndex = Math.floor(Math.random() * result.piadas.length);
+  } while (randomRiddleIndex === activeRiddle);
+
+  activeRiddle = result.piadas[randomRiddleIndex];
 
   for (const campo in result) {
     let riddleQuestion = activeRiddle[0];
     let riddleAnswer = activeRiddle[1];
 
     question.innerHTML = riddleQuestion;
-    answer.innerHTML = riddleAnswer;
+    answer.innerHTML = `
+    <span>.</span><span>.</span><span>.</span>
+    `;
+    timer = setTimeout(() => {
+      answer.innerHTML = riddleAnswer;
+      answer.classList.add("u-transition-append");
+    }, 3000);
   }
+  clearDisplay();
 };
 
-const removeClass = ()=>{
+const removeClass = () => {
   setTimeout(() => {
-    question.classList.remove('u-transition');
-    answer.classList.remove('u-transition');
+    question.classList.remove("u-transition-append");
+    answer.classList.remove("u-transition-append");
   }, 400);
+};
+
+const clearDisplay = ()=>{
+  timer2 = setTimeout(() => {
+    question.innerHTML = "...";
+    answer.innerHTML = "...";
+  }, 10000)
 }
 
 button.addEventListener("click", (e) => {
@@ -29,7 +50,8 @@ button.addEventListener("click", (e) => {
     })
     .catch((e) => console.error(e.message));
 
-    question.classList.add('u-transition');
-    answer.classList.add('u-transition');
-    removeClass();
+  question.classList.add("u-transition-append");
+  clearTimeout(timer2);
+  removeClass();
+  
 });
